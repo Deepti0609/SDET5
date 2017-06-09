ac4257edb8294e6ea82359f9fe44e9d190c58a54
deepti <p.deeptireddy@gmail.com>
2017-06-09 16:22:56 -0400

    HelloWorld Java Program

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package com.fannie;
+
+public class HelloWorld{
+
+public static void main(String args[]){
+
+System.out.println("This is from the notepad++");
+
+for(int i=0;i<10;i+=2){
+
+System.out.println(i);
+
+
+}
+
+}
+
+}
